0010ffa79bf68a608b180bdfeec2057f977c4063
Pearl <alexander.multan>
2022-03-07 20:25:32 +0300

    changes in 2.py

--- 2.py
+++ 2.py
@@ -24,7 +24,7 @@ people = [ {'name': 'Anna', 'age': 25, 'gender': 'female'},
     {'name': 'Fedor', 'age': 38, 'gender': 'male'},
     {'name': 'Nikita', 'age': 32, 'gender': 'male'},
     {'name': 'Alex', 'age': 25, 'gender': 'male'},
-    {'name': 'Foma', 'age': 7, 'gender': 'male'},
+    {'name': 'Foma', 'age': 70, 'gender': 'male'},
     {'name': 'Evgeniy', 'age': 25, 'gender': 'male'},
     {'name': 'Alex', 'age': 12, 'gender': 'male'},
     {'name': 'Kiril', 'age': 27, 'gender': 'male'},
@@ -32,6 +32,85 @@ people = [ {'name': 'Anna', 'age': 25, 'gender': 'female'},
     ]
 my_dict_length = len (people)
 print(f'Всего людей {my_dict_length}')
+
+
+print()
+
+
+girls = 0
+for i in range(0,30):
+    if people[i]['gender'] == 'female':
+        girls += 1
+print(f"Женщин {girls}")
+
+print()
+
+males = 0
+for a in range(0,30):
+    if people[a]['gender'] == 'male':
+        males += 1
+print(f'Мужчин {males}')
+
+print()
+
+adult = 0
+for a in range(0,30):
+    if people[a]['age'] >= 18:
+        adult += 1
+print(f'Совершеннолетних {adult}')
+
+print()
+
+names = []
+for a in range(0,30):
+    names.append(people[a]['name'])
+print(*names, sep='\n')
+
+print()
+
+ages = []
+for a in range(0,30):
+    if people[a]['age'] in ages:
+        pass
+    else:
+        ages.append(people[a]['age'])
+print(*ages, sep='\n')
+
+print()
+
+
+popularname = []
+checked = []
+count = {}
+for a in range(0,30):
+    if names[a] in checked:
+        pass
+    else:
+        repeats = names.count(names[a])
+        checked.append(names[a])
+        count[names[a]] = repeats
+
+for a in range(0,30):
+    if names[a] in popularname:
+        pass
+    else:
+        if count[names[a]] >= 3:
+            popularname.append(names[a])
+print(*popularname , sep='\n')
+
+print()
+
+for a in range(0,30):
+    if people[a]['age'] >= 35 and people[a]['gender'] == 'male' and people[a]['name'][0] == 'F':
+        print(people[a]['name'])
+
+
+
+
+    
+
+
+
 
 
 
