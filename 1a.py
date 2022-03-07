0eeb75cd30f6ab7a019fd38ea1803dd2c6ce13bb
Pearl <alexander.multan>
2022-03-07 20:59:06 +0300

    changes in 1a.py

--- 1a.py
+++ 1a.py
@@ -1,1 +1,7 @@
-katet1 = input()
+par1 = [a for a in input().split()]
+par2 = [a for a in input().split()]
+
+for a in range(0,len(par1)):
+    gipotinuza = (int(par1[a])**2+int(par2[a])**2)**0.5
+    ploshad = int(par1[a])*int(par2[a])/2
+    print(f'Треугольник {a + 1} с катетами {int(par1[a])} and {int(par2[a])} имеет S {ploshad} and гипотенузу {gipotinuza}')
